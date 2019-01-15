f6a2acc001b6f9a6e4e948087434295600f22d5b
JonLePotatoPhone <jcdiem3@gmail.com>
2019-01-15 17:09:23 -0600

    Going to remake and try to use tkinter instead of pygame

--- Chicken3.py
+++ Chicken3.py
@@ -19,30 +19,25 @@ def button(text, x, y, width, height, txtSize, inColor, activateColor, action=No
                 action()
             else:
                 action(param)
-##            print("You are clicking")
     else:
         smallText = pygame.font.SysFont(None,txtSize)
         textSurf, textRect = text_objects(text, smallText, GREY)
         textRect.center = ( (x+(width/2)), (y+(height/2)) )
     screen.blit(textSurf, textRect)
 
-##def switchScreen(scene):
-##    currentScene = scene
-##    print("Scene changed to {}".format(scene))
-##    #Information screen
-##    if (scene == 2):
-##        screen.fill((255,100,100))
-##        currentScene = 2
-##    print(currentScene)
+def switchScreen(scene):
+    global currentScene
+    currentScene = scene
+    print("Scene changed to {}".format(scene))
 
-def startGame(): #(whereFrom): Trying to build to allow for later implementation of contuing game from main menu after already starting the game once
-    screen.fill((255,100,100))
+##def startGame(): #(whereFrom): Trying to build to allow for later implementation of contuing game from main menu after already starting the game once
+##    screen.fill((255,100,100))
     
     
     
 pygame.init()    
 
-resX = 1024
+resX = 1024 #Will eventually allow user input for these values and use these as defaults
 resY = 576
 
 #Commonly used colors
@@ -61,34 +56,31 @@ screen = pygame.display.set_mode((resX,resY))
 logo = pygame.image.load(".\\art\\pixel\\logo32x32.png")
 pygame.display.set_icon(logo)
 pygame.display.set_caption("Chicken: The Threequel")
-
-#Other image loading
-##testImg = pygame.image.load(".\\art\\pixel\\pain.png")
-
-#Blits
-##screen.blit(testImg,(50,50))
  
 
 #Display stuff
 pygame.display.flip()
 
 #"First Run" stuff
-##currentScene = 1
+global currentScene
+currentScene = 1
 print("Setting initial scene to main menu")
-mMenu = True
 
 running = True
 try: #Using a try statement so the window actually closes
     while running:
         ##Main active here##
-        
+##        global currentScene # Using a global for now, will find a way to just pass it through... eventualy
         #Menu Scene
         pygame.event.pump()
-        if mMenu:
+        if currentScene == 1:
             screen.fill(BLACK)
-            mMenu = button("Start",150,100,250,250,150,(100,100,100),(50,50,50),startGame)
-##            print(currentScene)
-        
+            button("Start",150,100,250,250,150,(100,100,100),(50,50,50),switchScreen,2)
+        if currentScene == 2:
+            print("Change")
+            screen.fill(BLACK)
+            
+            button("Start",150,100,250,250,150,(100,100,100),(50,50,50),switchScreen,3)
         pygame.display.update()
         #Closing the box
         for event in pygame.event.get():
