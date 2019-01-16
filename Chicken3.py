c91a305e652eec87ee8d8674640e946257b98450
JonLePotatoPhone <jcdiem3@gmail.com>
2019-01-16 15:31:33 -0600

    Pushing work on beginning of main menu so I can work abroad

--- Chicken3.py
+++ Chicken3.py
@@ -1,7 +1,5 @@
 from tkinter import *
 #import csv
-from random import randint
-
 
 #Tk init
 root = Tk()
@@ -15,25 +13,29 @@ if (scrAsp > 1.68 and scrAsp < 1.8): #Looking for 1.77
     resX = int(16 * (0.75*(scrX/16)))
     resY = int(9 * (0.75*(scrY/9)))
 elif (scrAsp > 1.58 and scrAsp < 1.70): #Looking for 1.6
-    resX = int( 16 * (0.25*scrX))
-    resY = int( 10 * (0.25*scrY))
+    resX = int( 16 * (0.75*(scrX/16)))
+    resY = int( 10 * (0.75*(scrY/10)))
 else: #Resolution for peasents or people with weird tastes
     resX = 640
-    resyY = 480
+    resY = 480
 resXY = str(resX)+'x'+str(resY)
 print("Screen x is {}".format(scrX))
 print("Screen y is {}".format(scrY))
-print("Res y is {}".format(resY))
-print("Res x is {}".format(resX))
+print("Game y is {}".format(resY))
+print("Game x is {}".format(resX))
 Grid.rowconfigure(root, 0, weight=1)
 Grid.columnconfigure(root, 0, weight=1)
 root.geometry(resXY)
 root.iconbitmap(r'.\art\pixel\favicon.ico')
 
 #Setting up main screen
+mainFrame = Frame(root,)
 def startMenu():
-    frame = Frame(root)
-    frame.grid(row=0,column=0,sticky=N+S+E+W)
+    startGameBtn = Button(mainFrame, text="Start Game", command=loadInfoScreen)
+    startGameBtn.grid()
+
+def loadInfoScreen():
+    return
 
 #Init stuff
 startMenu()
