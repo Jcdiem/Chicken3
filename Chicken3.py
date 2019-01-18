e4214ef9bf1ec8885f6eb929719c29ef48473a1c
JonLePotatoPhone <jcdiem3@gmail.com>
2019-01-18 11:55:12 -0600

    Actually fixed the main menu again -- probably

--- Chicken3.py
+++ Chicken3.py
@@ -16,18 +16,18 @@ resY = 0
 scrAsp = float(scrX/scrY)
 if (scrAsp > 1.68 and scrAsp < 1.8): #Looking for 1.77
     resX = int(16 * (0.75*(scrX/16)))
-    resY = int(9 * (0.75*(scrY/9)))
+    resY = int( 9 * (0.75*(scrY/ 9)))
 elif (scrAsp > 1.58 and scrAsp < 1.70): #Looking for 1.6
-    resX = int( 16 * (0.75*(scrX/16)))
-    resY = int( 10 * (0.75*(scrY/10)))
+    resX = int(16 * (0.75*(scrX/16)))
+    resY = int(10 * (0.75*(scrY/10)))
 else: #Resolution for peasents or people with weird tastes
     resX = 640
     resyY = 480
 resXY = str(resX)+'x'+str(resY)
 print("Screen x is {}".format(scrX))
 print("Screen y is {}".format(scrY))
-print("Res y is {}".format(resY))
 print("Res x is {}".format(resX))
+print("Res y is {}".format(resY))
 tk.Grid.rowconfigure(root, 0, weight=1)
 tk.Grid.columnconfigure(root, 0, weight=1)
 root.geometry(resXY)
@@ -39,18 +39,21 @@ def startGame():
 
 #Setting up intro screen
 def startMenu():
+    def urMom():
+        print("ur mom dumb")
+        return
     frame = tk.Frame(root,width=200,height=200,bg='black')
     frame.grid(row=0,column=0, sticky="nsew")
     frame.grid_rowconfigure(0, weight = 1)
     frame.grid_columnconfigure(0, weight = 1)
     #Fonts
     menuSizeMedium = round(0.0000423*(resX*resY))
-    menuBtnFont = ('TkDefaultFont',menuSizeMedium)
+    menuBtnFont = ('comic sans',menuSizeMedium)
 
-    menuSizeLarge = 50
-    menuTitleFont = ('TkDefaultFont',menuSizeLarge)
+    menuSizeLarge = round(0.00006887*(resX*resY))
+    menuTitleFont = ('comic sans',menuSizeLarge)
     #Buttons
-    startGameBtn = tk.Button(frame, font=menuBtnFont, text="Start Game",action=startGame(),bg=(_from_rgb((50,50,50))),foreground='white smoke',activebackground='gray58',activeforeground='white smoke')
+    startGameBtn = tk.Button(frame, font=menuBtnFont, text="Start Game",command=urMom,bg=(_from_rgb((50,50,50))),foreground='white smoke',activebackground='gray58',activeforeground='white smoke')
     
     #Lables / Titles
     mMenuTitle = tk.Label(frame, font=menuTitleFont, text ="Chicken: The Threequel", fg='white smoke',bg=('black'))
